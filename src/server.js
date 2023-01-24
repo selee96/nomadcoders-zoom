a493a2f7a1cf0340bded93df3be4ec699d4d27a7
selee <tldms296@gmail.com>
2023-01-24 20:51:37 +0900

    chat

--- src/server.js
+++ src/server.js
@@ -15,4 +15,26 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser ✅");
+    socket.on("close", onSocketClose);
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }        
+    });
+});
+
 server.listen(3000, handleListen);
